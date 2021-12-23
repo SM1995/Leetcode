308cc97db173da63e731cef0d24bc959974a160d
Shraddha Mishra <7471821+shraddhamishra7@users.noreply.github.com>
2021-12-23 11:29:11 -0500

    Update 344. Reverse String.java

--- Two Pointers/344. Reverse String.java
+++ Two Pointers/344. Reverse String.java
@@ -8,5 +8,26 @@ class Solution {
             s[i] = s[n-i];
             s[n-i] = temp;
         }
+        
+        /*
+        //Approach 2:
+        class Solution {
+            public void reverseString(char[] s) {
+                helper(s,0,s.length-1);
+
+            }
+
+            public void helper(char[] s, int start, int end){
+
+                if(start>=end) return;
+
+                char temp = s[start];
+                s[start] = s[end];
+                s[end] = temp;
+                helper(s, start+1, end-1);
+            }
+        }
+        */
     }
+    
 }
